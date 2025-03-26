f55b569dcdcfd11d7d6c5660a886f0483bc4bd6f
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-26 14:10:02 +0100

    chore: format with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-mod utils;
 mod algorithms;
+mod utils;
 
-use serde_json::{json, Value};
 use crate::algorithms::hs256::jwt_encode as hs256_encode;
 use crate::algorithms::hs256::jwt_verify_and_decode as hs256_decode;
+use serde_json::{json, Value};
 
-fn main(){
+fn main() {
     let header = json!({
         "alg": "HS256",
         "typ": "JWT"
@@ -17,5 +17,10 @@ fn main(){
     });
 
     println!("{}", hs256_encode(&header, &payload));
-    println!("{}", hs256_decode(" ".to_string()));
+    println!(
+        "{}",
+        hs256_decode(String::from(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6InRydWUiLCJuYW1lIjoicmFuZG8ifQ"
+        ))
+    );
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,14 +1,14 @@
 use base64::{engine::general_purpose, Engine as _};
 use serde_json::Value;
 
-pub fn b64(input: &Value) -> String{
+pub fn b64(input: &Value) -> String {
     let json_string = serde_json::to_string(&input).unwrap();
     general_purpose::URL_SAFE_NO_PAD.encode(json_string.as_bytes())
 }
 
-pub fn unb64(value: &str) -> Value{
+pub fn unb64(value: &str) -> Value {
     let decoded_bytes = general_purpose::URL_SAFE_NO_PAD.decode(value).unwrap();
     let decoded_str = String::from_utf8(decoded_bytes).unwrap();
-    let json_value: Value =  serde_json::from_str(&decoded_str).unwrap();
+    let json_value: Value = serde_json::from_str(&decoded_str).unwrap();
     json_value
 }
