cd2342c6fbcb3e81adde89c33cbf0f8dfde34ea3
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-26 14:23:15 +0100

    chore(hs256): remove last "." from jwt_encode

--- src/algorithms/hs256.rs
+++ src/algorithms/hs256.rs
@@ -9,7 +9,7 @@ pub fn jwt_encode(header: &Value, payload: &Value) -> String {
     let encoded_header = utils::b64(header);
     let encoded_payload = utils::b64(payload);
 
-    format!("{}.{}.", encoded_header, encoded_payload)
+    format!("{}.{}", encoded_header, encoded_payload)
 }
 
 pub fn jwt_verify_and_decode(jwt: String) -> Value {
@@ -21,15 +21,18 @@ pub fn jwt_verify_and_decode(jwt: String) -> Value {
     if parts.len() != 2 {
         utils::error_and_exit("Invalid jwt format");
     }
+
     let header = utils::unb64(parts[0]);
     if header["alg"] != "HS256" {
         let msg = format!("Wrong algorithm: {}", header["alg"]);
         utils::error_and_exit(&msg);
     }
+
     let payload = utils::unb64(parts[1]);
 
     return json!({
         "header": header,
-        "payload": payload
+        "payload": payload,
+        "valid" : true
     });
 }
