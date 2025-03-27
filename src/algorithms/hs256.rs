9d567491161c723d9d6adda3a90ec3fabd755b9a
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-27 14:34:17 +0100

    chore: massive refactoring

--- src/algorithms/hs256.rs
+++ src/algorithms/hs256.rs
@@ -2,7 +2,7 @@ use crate::utils;
 use serde_json::{json, Value};
 use ring::hmac::{self, HMAC_SHA256};
 
-pub fn jwt_encode(header: &Value, payload: &Value, secret: String) -> String {
+pub fn jwt_encode(header: &Value, payload: &Value, secret: &str) -> String {
     if !header.is_object() || !payload.is_object() {
         utils::error_and_exit("header and payload must be json objects");
     }
@@ -20,23 +20,29 @@ pub fn jwt_encode(header: &Value, payload: &Value, secret: String) -> String {
     format!("{}.{}.{}", encoded_header, encoded_payload, signature)
 }
 
-pub fn jwt_verify_and_decode(jwt: String) -> Value {
-    if jwt.trim().is_empty() {
+pub fn jwt_verify_and_decode(jwt: String, secret: String) -> Value {
+    if jwt.trim().is_empty() || secret.trim().is_empty() {
         utils::error_and_exit("empty string detected");
     }
 
-    let parts: Vec<_> = jwt.split('.').collect();
-    if parts.len() != 2 {
+    let parts: Vec<String> = jwt.split('.').map(String::from).collect();
+    if parts.len() != 3 {
         utils::error_and_exit("Invalid jwt format");
     }
 
-    let header = utils::unb64(parts[0]);
+    let header: Value = serde_json::from_str(&utils::unb64(&parts[0])).unwrap();
     if header["alg"] != "HS256" {
         let msg = format!("Wrong algorithm: {}", header["alg"]);
         utils::error_and_exit(&msg);
     }
 
-    let payload = utils::unb64(parts[1]);
+    let payload: Value = serde_json::from_str(&utils::unb64(&parts[1])).unwrap() ;
+    let jwt_unprotected = format!("{}.{}", parts[0], parts[1]);
+
+
+    let signature = hs256(jwt_unprotected, &secret);
+
+    let valid = signature == parts[2];
 
     return json!({
         "header": header,
@@ -45,9 +51,9 @@ pub fn jwt_verify_and_decode(jwt: String) -> Value {
     });
 }
 
-pub fn hs256(unsigned_jwt: String, secret: String) -> String {
+pub fn hs256(jwt_unprotected: String, secret: &str) -> String {
     let key = hmac::Key::new(HMAC_SHA256, secret.as_bytes());
-    let signature = hmac::sign(&key, &unsigned_jwt.as_bytes());
+    let signature = hmac::sign(&key, &jwt_unprotected.as_bytes());
 
     utils::b64(signature.as_ref())
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,16 +1,13 @@
 use base64::{engine::general_purpose, Engine as _};
-use serde_json::Value;
 use std::process;
 
-pub fn b64(input: &[u8] ) -> String {
+pub fn b64(input: &[u8]) -> String {
     general_purpose::URL_SAFE_NO_PAD.encode(input)
 }
 
-pub fn unb64(value: &str) -> Value {
+pub fn unb64(value: &str) -> String {
     let decoded_bytes = general_purpose::URL_SAFE_NO_PAD.decode(value).unwrap();
-    let decoded_str = String::from_utf8(decoded_bytes).unwrap();
-    let json_value: Value = serde_json::from_str(&decoded_str).unwrap();
-    json_value
+    String::from_utf8(decoded_bytes).unwrap()
 }
 
 pub fn error_and_exit(msg: &str) {
