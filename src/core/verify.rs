398f2db647bec671ae8745d6e5443b979f15f472
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-28 07:32:14 +0100

    feat: create global verify function

---
+++ src/core/verify.rs
@@ -0,0 +1,11 @@
+use crate::{algorithms::hs256::jwt_verify_and_decode as hs256_decode, utils};
+use crate::utils::error::JwtError;
+use serde_json::Value;
+
+
+pub fn verify(signed: &str, secret: &str, algorithm: &str) -> Result<Value, JwtError> {
+    match algorithm {
+        "HS256" => hs256_decode(signed, secret),
+        _ => Err(utils::error::JwtError::UnsupportedAlgorithm(algorithm.to_owned()))
+    }
+}
