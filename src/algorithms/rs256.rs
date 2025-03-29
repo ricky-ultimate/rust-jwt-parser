a70456b31914a2e42d2fc75d1bf7d65352b2fc2c
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-29 16:47:04 +0100

    feat(rs256): properly implement rs256 encryption logic

--- src/algorithms/rs256.rs
+++ src/algorithms/rs256.rs
@@ -1,16 +1,41 @@
-use ring::{rand::SystemRandom, signature::{self, RSA_PKCS1_SHA256}};
-
 use crate::utils;
+use ring::rand::SystemRandom;
+use ring::signature::{RsaKeyPair, RSA_PKCS1_SHA256};
 
+pub fn rs256(jwt_unprotected: &str, private_key_pem: &str) -> String {
+    let private_key_der = pem_to_der(private_key_pem).expect("Failed to convert PEM to DER");
 
-pub fn rs256(jwt_unprotected: &str, private_key: &str) -> String {
-    let private_key_as_bytes = private_key.as_bytes();
-    let private_key = signature::RsaKeyPair::from_pkcs8(&private_key_as_bytes).expect("Error");
+    let private_key =
+        RsaKeyPair::from_pkcs8(&private_key_der).expect("Invalid RSA private key format");
 
-    let rng= SystemRandom::new();
+    let rng = SystemRandom::new();
     let mut signature = vec![0; private_key.public_modulus_len()];
 
-    private_key.sign(&RSA_PKCS1_SHA256, &rng, jwt_unprotected.as_bytes(), &mut signature).expect("msg");
+    private_key
+        .sign(
+            &RSA_PKCS1_SHA256,
+            &rng,
+            jwt_unprotected.as_bytes(),
+            &mut signature,
+        )
+        .expect("RSA signing failed");
 
     utils::b64(signature.as_ref())
+}
+
+fn pem_to_der(pem: &str) -> Result<Vec<u8>, &'static str> {
+    let pem = pem.trim();
+    if pem.starts_with("-----BEGIN") {
+        let base64_part = pem
+            .lines()
+            .filter(|line| !line.starts_with("-----"))
+            .collect::<Vec<&str>>()
+            .join("");
+
+        utils::unb64(&base64_part)
+            .map(|decoded_str| decoded_str.into_bytes())
+            .map_err(|_| "Failed to decode base64")
+    } else {
+        Err("Invalid PEM format")
+    }
 }
