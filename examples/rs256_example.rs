5b2f8694c4003bbc9706f3909b0d3a157e2064aa
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-29 20:28:24 +0100

    chore(rs256): add example for verify and decode

--- examples/rs256_example.rs
+++ examples/rs256_example.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use rust_jwt_parser::algorithms::rs256::jwt_encode;
+use rust_jwt_parser::algorithms::rs256::{jwt_encode, jwt_verify_and_decode};
 use serde_json::{json, Value};
 use std::env;
 use std::fs;
@@ -25,5 +25,12 @@ fn main() {
     match jwt_encode(&header, &payload, &private_key) {
         Ok(token) => println!("jwt: {}", token),
         Err(e) => eprintln!("{}", e),
+    }
+
+
+    let jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzQzMzYxODU1LCJuYW1lIjoiSm9obiBEb2UiLCJzdWIiOiIxMjM0NTY3ODkwIn0.nQ5RbTqw_bfIjL3fwngBBNfDMjAvTTcMgkiZKwOhm0HpGg2BPOhHo8NFAjMYEFhMqlf0jw_lv7QK4lsaPvy0USaWkf0mQAJoASvrftMfCl9LVGmZbDqaGPpwPCtvpmREWRTMINWEjAaCE91MQDJlDhMmClYrfxvNGvSnctiYNFKZbnpWsvAWJcAdWVZrPmTTfadk1b_AZvyv-9xml22jB8w60ONNg32ybGKQb36V_1UQr9X_eO6ijc26OPMZY5z6xXgKkJb_PgLgsMAxSJKKMbiwUC4_OOaDwqvCoQlbJYaYTMpbMjmg9rodg46SX3XVTRG1YCRCZ-bTmyCl0Ce7-g";
+    match jwt_verify_and_decode(jwt, &private_key){
+        Ok(decoded) => println!("Decoded JWT: {}", decoded),
+        Err(e) => eprintln!("Error decoding JWT: {}", e),
     }
 }
