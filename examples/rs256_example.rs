261d36f51c43ca3bc3ef7227aa5744133440efd2
Ricky Ultimate <rickyogigi@gmail.com>
2025-03-30 20:45:28 +0100

    feat(rs256): directly pass encoded token into decode function

--- examples/rs256_example.rs
+++ examples/rs256_example.rs
@@ -20,17 +20,18 @@ fn main() {
     });
 
     let private_key_path = env::var("RSA_PRIVATE_KEY_PATH").expect("RSA_PRIVATE_KEY_PATH must be set");
-    let private_key = fs::read_to_string(private_key_path).expect("Failed to read private key file");
+    let private_key_pem = fs::read_to_string(private_key_path).expect("Failed to read private key file");
+    let public_key_path = env::var("RSA_PUBLIC_KEY_PATH").expect("RSA_PUBLIC_KEY_PATH must be set");
+    let public_key_pem = fs::read_to_string(public_key_path).expect("Failed to read private key file");
 
-    match jwt_encode(&header, &payload, &private_key) {
-        Ok(token) => println!("jwt: {}", token),
+    match jwt_encode(&header, &payload, &private_key_pem) {
+        Ok(token) => {
+            println!("jwt: {}", token);
+            match jwt_verify_and_decode(&token, &public_key_pem){
+                Ok(decoded) => println!("Decoded JWT: {}", decoded),
+                Err(e) => eprintln!("Error decoding JWT: {}", e),
+            }
+        },
         Err(e) => eprintln!("{}", e),
-    }
-
-
-    let jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzQzMzYxODU1LCJuYW1lIjoiSm9obiBEb2UiLCJzdWIiOiIxMjM0NTY3ODkwIn0.nQ5RbTqw_bfIjL3fwngBBNfDMjAvTTcMgkiZKwOhm0HpGg2BPOhHo8NFAjMYEFhMqlf0jw_lv7QK4lsaPvy0USaWkf0mQAJoASvrftMfCl9LVGmZbDqaGPpwPCtvpmREWRTMINWEjAaCE91MQDJlDhMmClYrfxvNGvSnctiYNFKZbnpWsvAWJcAdWVZrPmTTfadk1b_AZvyv-9xml22jB8w60ONNg32ybGKQb36V_1UQr9X_eO6ijc26OPMZY5z6xXgKkJb_PgLgsMAxSJKKMbiwUC4_OOaDwqvCoQlbJYaYTMpbMjmg9rodg46SX3XVTRG1YCRCZ-bTmyCl0Ce7-g";
-    match jwt_verify_and_decode(jwt, &private_key){
-        Ok(decoded) => println!("Decoded JWT: {}", decoded),
-        Err(e) => eprintln!("Error decoding JWT: {}", e),
     }
 }
